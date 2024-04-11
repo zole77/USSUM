f6228159e9a557611162443f5537059b44d8a827
yunseok kim <skyblue0728@daum.net>
2024-04-11 10:54:24 +0900

    project

---
+++ src/test/java/com/mycom/ussum/UssumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycom.ussum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UssumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
