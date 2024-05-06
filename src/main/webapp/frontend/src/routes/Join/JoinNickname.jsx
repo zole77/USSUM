126ea7da70a5b30ab2c3419ffb3eaced11d1cd49
bomi Kim <withpisces1248@gmail.com>
2024-05-06 18:19:27 +0900

    bom button edit test

--- src/main/webapp/frontend/src/routes/Join/JoinNickname.jsx
+++ src/main/webapp/frontend/src/routes/Join/JoinNickname.jsx
@@ -2,8 +2,8 @@ import React, { useContext, useState } from "react";
 import { LabelContext } from "./labelDataContext";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import ButtonGroup from "@mui/material/ButtonGroup";
 import Grid from "@mui/material/Grid";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack"; // 화살표 아이콘 추가
 import "../../styles/Join_styles.css";
 
 const JoinNickname = () => {
@@ -33,55 +33,59 @@ const JoinNickname = () => {
   };
 
   return (
-    <form>
-      <h4>회원가입</h4>
-      <Grid container spacing={2} alignItems="center">
-        <Grid item xs={9}>
-          <TextField
-            label="닉네임을 입력해주세요"
-            style={{ width: "100%" }}
-            fullWidth
-            required
-            onChange={(event) => value.setJoinNicknameInfo("nickname")(event)}
-            value={joinNickname ? joinNickname.nickname : ""}
-          />
+      <form>
+        <Grid container spacing={2} alignItems="center">
+          <Grid item xs={3}>
+            <Button
+                onClick={() => value.prevPage()}
+                style={{ marginRight: "10px", marginTop: "5px" }} // 화살표를 약간 아래로 내립니다.
+                className="join-btn-hover color"
+            >
+              <ArrowBackIcon />
+            </Button>
+          </Grid>
+          <Grid item xs={9}>
+            <h4 style={{ marginBottom: 15 }}>회원가입</h4>
+          </Grid>
+          <Grid item xs={3}></Grid> {/* 회원가입 왼쪽의 버튼 영역과 동일한 공간을 만듭니다. */}
+          <Grid item xs={9}></Grid> {/* 회원가입 왼쪽의 버튼 영역과 동일한 공간을 만듭니다. */}
+          <Grid item xs={9}>
+            <TextField
+                label="닉네임을 입력해주세요"
+                style={{ width: "100%" }}
+                fullWidth
+                required
+                onChange={(event) => value.setJoinNicknameInfo("nickname")(event)}
+                value={joinNickname ? joinNickname.nickname : ""}
+            />
+          </Grid>
+          <Grid item xs={3}>
+            <Button
+                onClick={handleCheckDuplicate}
+                variant="contained"
+                color="primary"
+                style={{ marginLeft: "10px", height: "56px" }}
+                className="join-btn-hover color"
+            >
+              중복 확인
+            </Button>
+          </Grid>
+          <Grid item xs={3}></Grid> {/* 회원가입 왼쪽의 버튼 영역과 동일한 공간을 만듭니다. */}
+          <Grid item xs={9}>
+            <div style={{ marginTop: 15 }}>
+              <Button
+                  disabled={isDuplicateChecked} // 중복 확인 후 결과값이 true일 때만 버튼 비활성화.
+                  onClick={() => value.nextPage()}
+                  variant="contained"
+                  color="primary"
+                  className="join-btn-hover color"
+              >
+                다음
+              </Button>
+            </div>
+          </Grid>
         </Grid>
-        <Grid item xs={3}>
-          <Button
-            onClick={handleCheckDuplicate}
-            variant="contained"
-            color="primary"
-            style={{ marginLeft: "10px", height: "56px" }}
-            className="join-btn-hover color"
-          >
-            중복 확인
-          </Button>
-        </Grid>
-      </Grid>
-
-      <ButtonGroup
-        variant="contained"
-        color="primary"
-        aria-label="text primary button group"
-        style={{ marginTop: 15 }}
-      >
-        <Button
-          onClick={() => value.prevPage()}
-          style={{ margin: 25 }}
-          className="join-btn-hover color"
-        >
-          이전
-        </Button>
-        <Button
-          disabled={isDuplicateChecked} // 중복 확인 후 결과값이 true일 때만 버튼 비활성화.
-          onClick={() => value.nextPage()}
-          style={{ margin: 25 }}
-          className="join-btn-hover color"
-        >
-          다음
-        </Button>
-      </ButtonGroup>
-    </form>
+      </form>
   );
 };
 
