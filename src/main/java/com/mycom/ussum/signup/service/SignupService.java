75cedcaf5ffe424d6196ba2cffef89d199bf598c
SuHyeon <tngus389@gmail.com>
2024-05-08 10:32:45 +0900

    회원가입

--- src/main/java/com/mycom/ussum/signup/service/SignupService.java
+++ src/main/java/com/mycom/ussum/signup/service/SignupService.java
@@ -1,4 +1,10 @@
 package com.mycom.ussum.signup.service;
 
+import com.mycom.ussum.signup.vo.SignupVO;
+
 public interface SignupService {
+
+    boolean signUp(SignupVO signupVO);
+    int checkById(String mem_id);
+    int checkByNickname(String mem_nickname);
 }
--- src/main/java/com/mycom/ussum/signup/service/SignupServiceImpl.java
+++ src/main/java/com/mycom/ussum/signup/service/SignupServiceImpl.java
@@ -1,4 +1,43 @@
 package com.mycom.ussum.signup.service;
 
-public class SignupServiceImpl {
+import com.mycom.ussum.signup.dao.SignupDAO;
+import com.mycom.ussum.signup.vo.SignupVO;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SignupServiceImpl implements SignupService{
+
+    private final SignupDAO signupDAO;
+
+    @Autowired
+    public SignupServiceImpl(SignupDAO signupDAO){
+        this.signupDAO = signupDAO;
+    }
+
+    @Override
+    public boolean signUp(@NotNull SignupVO signupVO){
+        int checkById = signupDAO.checkById(signupVO.getMem_id());
+
+        int checkByNickname = signupDAO.checkByNickname(signupVO.getMem_nickname());
+
+        if (checkById == 0 && checkByNickname == 0) {
+            signupDAO.signUp(signupVO);
+            return true; // 회원 가입 성공
+        } else {
+            return false; // 회원 가입 실패
+        }
+    }
+
+    @Override
+    public int checkById(String mem_id) {
+
+        return signupDAO.checkById(mem_id);
+    }
+    @Override
+    public int checkByNickname(String mem_nickname) {
+
+        return signupDAO.checkByNickname(mem_nickname);
+    }
 }
