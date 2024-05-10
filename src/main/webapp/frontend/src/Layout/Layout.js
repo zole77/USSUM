cbbfed2819454d05ee9635654058108c5aa4943c
bomi Kim <withpisces1248@gmail.com>
2024-05-10 15:24:50 +0900

    save

--- src/main/webapp/frontend/src/Layout/Layout.js
+++ src/main/webapp/frontend/src/Layout/Layout.js
@@ -1,16 +1,19 @@
 import React from "react";
-import Header from "./Header.js";
-import Footer from "./Footer.js";
-import Body from "./Body.js";
+import Header from "./Header";
+import Footer from "./Footer";
+import Body from "./Body";
+import "../styles/Layout.css";
 
 function Layout(props) {
-  return (
-    <div>
-      <Header />
-      <Body />
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="layout-container">
+            <Header />
+            <div className="body-container">
+                <Body />
+            </div>
+            <Footer />
+        </div>
+    );
 }
 
 export default Layout;
