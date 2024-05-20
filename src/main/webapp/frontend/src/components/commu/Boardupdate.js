84c2536df5fe281e785648e4c6545c53a57b2dbb
박범수 <qjatn8595@gmail.com>
2024-05-20 16:40:14 +0900

    게시글 등록 기능 추가

--- src/main/webapp/frontend/src/components/commu/Boardupdate.js
+++ src/main/webapp/frontend/src/components/commu/Boardupdate.js
@@ -1,6 +1,7 @@
 import React, { useRef } from "react";
 import { Editor, Viewer } from "@toast-ui/react-editor";
 import "@toast-ui/editor/toastui-editor.css";
+import { IoIosClose } from "react-icons/io";
 import "../../App.css";
 
 function Boardupdate(props) {
@@ -12,7 +13,7 @@ function Boardupdate(props) {
         alert(content);
     };
 
-    let find = props.boardList.find((x) => x.id === props.postId);
+    let find = props.boardList.find((x) => x.post_no === props.postId);
 
     return (
         <div>
@@ -22,17 +23,44 @@ function Boardupdate(props) {
                     ref={modalBackground}
                     onClick={(e) => {
                         if (e.target === modalBackground.current) {
-                            props.setUpdateModalOpen(false);
+                            if (editorRef.current.getInstance().getMarkdown().trim()) {
+                                if (
+                                    window.confirm(
+                                        "작성 중인 내용이 있습니다. 정말로 닫으시겠습니까?"
+                                    )
+                                ) {
+                                    props.setUpdateModalOpen(false);
+                                }
+                            } else {
+                                props.setUpdateModalOpen(false);
+                            }
                         }
                     }}
                 >
                     <div className="modal-content" style={{ backgroundColor: "#fff" }}>
-                        <p>글 수정 모달창</p>
-                        <p>
+                        <p className="modal-header">
+                            USSUM 여행 커뮤니티
+                            <IoIosClose
+                                className="modal-x"
+                                onClick={() => {
+                                    props.setUpdateModalOpen(false);
+                                    document.body.style.overflow = "";
+                                }}
+                                style={{
+                                    cursor: "pointer",
+                                }}
+                            />
+                        </p>
+                        <div className="author-created">
+                            <p>{find.mem_id}</p>
+                            <p style={{ marginLeft: "auto" }}>{find.post_date}</p>
+                        </div>
+                        <p style={{ textAlign: "left" }}>
                             제목
                             <input
-                                defaultValue={find.title}
+                                defaultValue={find.post_title}
                                 style={{
+                                    marginLeft: "10px",
                                     marginBottom: "10px",
                                     width: "60%",
                                     height: "32px",
@@ -48,7 +76,7 @@ function Boardupdate(props) {
                             language="ko-KR"
                             ref={editorRef}
                             height="300px"
-                            initialValue={find.content}
+                            initialValue={find.post_content || " "}
                             initialEditType="wysiwyg"
                         />
                         <button
--- src/main/webapp/frontend/src/routes/Main.js
+++ src/main/webapp/frontend/src/routes/Main.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
-function Main() {
-  return (
-    <div>
-      <p>Main page</p>
 
-      <Link to="/signup">signup page</Link>
-      <br />
-      <Link to="/login">Login page</Link>
-      <br />
-      <Link to="/info">services</Link>
-    </div>
-  );
+function Main() {
+    return (
+        <div>
+            <p>Main page</p>
+
+            <Link to="/signup">signup page</Link>
+            <br />
+            <Link to="/login">Login page</Link>
+            <br />
+            <Link to="/info">services</Link>
+        </div>
+    );
 }
 
 export default Main;
