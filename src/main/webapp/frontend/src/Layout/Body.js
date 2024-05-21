48385734002c5b8821ef463ab3afe89be50ab3f8
박범수 <qjatn8595@gmail.com>
2024-05-21 18:19:34 +0900

    커뮤니티 글 수정, 삭제 오류있던 부분 수정 완료

--- src/main/webapp/frontend/src/Layout/Body.js
+++ src/main/webapp/frontend/src/Layout/Body.js
@@ -6,21 +6,23 @@ import Main from "../routes/Main";
 import Login from "../routes/Login/Login";
 import SignUp from "../routes/SignUp/SignUp";
 import ModMember from "../routes/MyPage/ModMember";
+import ChatRoom from "../routes/Chat/ChatRoom";
 
 function Body() {
-  return (
-    <div>
-      <Routes>
-        <Route path="/info" element={<ServiceIntro />} />
-        <Route path="/board" element={<Board />} />
-        <Route path="*" element={<div>404에러</div>} />
-        <Route path="/" element={<Main />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/mypage" element={<ModMember />} />
-      </Routes>
-    </div>
-  );
+    return (
+        <div>
+            <Routes>
+                <Route path="/info" element={<ServiceIntro />} />
+                <Route path="/board" element={<Board />} />
+                <Route path="*" element={<div>404에러</div>} />
+                <Route path="/" element={<Main />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/mypage" element={<ModMember />} />
+                <Route path="/chat" element={<ChatRoom />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default Body;
