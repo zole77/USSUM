3f4230a2891926ee253e4972a1f2bdf3b22f2159
yunseok kim <skyblue0728@daum.net>
2024-05-22 13:39:39 +0900

    로그인 오류 수정

--- src/main/java/com/mycom/ussum/login/controller/LoginController.java
+++ src/main/java/com/mycom/ussum/login/controller/LoginController.java
@@ -2,23 +2,25 @@ package com.mycom.ussum.login.controller;
 
 import com.mycom.ussum.login.service.LoginService;
 import com.mycom.ussum.login.vo.LoginVO;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
-
+@RequiredArgsConstructor
 public class LoginController {
 
     private final LoginService loginService;
 
-    @Autowired
-    public LoginController(LoginService loginService) {
-        this.loginService = loginService;
-    }
+//    @Autowired
+//    public LoginController(LoginService loginService) {
+//        this.loginService = loginService;
+//    }
 
     @GetMapping(value = "/login")
     public String showLoginForm(){
@@ -27,35 +29,35 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public void  doLogin(//@RequestParam("mem_id") String mem_id,
-                          //@RequestParam("mem_pwd") String mem_pwd,
-                          @RequestBody LoginVO loginVO,
+    public Map<String, String> doLogin(
+                          @RequestParam("mem_id") String mem_id,
+                          @RequestParam("mem_pwd") String mem_pwd,
                           RedirectAttributes redirectAttributes,
                           HttpSession session){
-        List<LoginVO> result = loginService.login(loginVO.getMem_id(), loginVO.getMem_pwd());
+        Map<String, String> map = new HashMap<>();
+        String result = loginService.login(mem_id, mem_pwd).getMem_id();
 
-        if (result == null || result.isEmpty()) {
+        if (result == null) {
             System.out.println("로그인이 안됨");
             redirectAttributes.addFlashAttribute("error", "회원 정보를 다시 확인바랍니다.");
+//            return "redirect:/login";
 
+            return map;
         } else {
-            for(LoginVO login : result){
-                if(login == null){
-                    System.out.println("결과 목록에서 Null LoginVO 발견");
-                    continue;
-                }
-            }
-            System.out.println(result.toString());
+//            System.out.println(result.getFirst().toString());
             // 로그인 성공 시 세션에 사용자 정보 저장
-            String sessionMemId = result.get(0).getMem_id();
-            session.setAttribute("mem_id", sessionMemId);
+            map.put("mem_id", result);
+            map.put("message", "LOGIN SUCCESS");
+            session.setAttribute("mem_id", result);
+            return map;
 
-
+//            return "redirect:/";
         }
     }
     @GetMapping("/logout")
     public String doLogout(HttpSession session){
-        session.removeAttribute("mem_id");
+        session.removeAttribute("mem_no");
+
         session.invalidate();
         System.out.println("세션삭제완료");
         return "redirect:/";
