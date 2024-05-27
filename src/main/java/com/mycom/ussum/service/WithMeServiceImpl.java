efcc583e1c0271f8e8955b12b9c48d997ee59025
박범수 <qjatn8595@gmail.com>
2024-05-27 10:03:11 +0900

    이미지 예외처리

--- src/main/java/com/mycom/ussum/service/WithMeServiceImpl.java
+++ src/main/java/com/mycom/ussum/service/WithMeServiceImpl.java
@@ -22,7 +22,7 @@ public class WithMeServiceImpl implements WithMeService {
 
     @Override
     public void createWithMe(WithMeVO withMeVO, MultipartFile image) throws IOException {
-        if(image.isEmpty()){
+        if(image == null){
             repo.createWithMe(withMeVO);
         } else {
             String fullFilePath = saveImage(image);
