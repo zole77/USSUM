874d25cdcc3c4321fa603418433ad0a1c2b3a064
SuHyeon <tngus389@gmail.com>
2024-05-28 21:25:51 +0900

    회원가입

--- src/main/webapp/frontend/src/routes/Withme/KakaoMap.js
+++ src/main/webapp/frontend/src/routes/Withme/KakaoMap.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 
-const KakaoMap = () => {
+
+const KakaoMap = ({selectedCity, selectedDistrict}) => {
     useEffect(() => {
         const kakaoMapScript = document.createElement('script')
         kakaoMapScript.async = false
@@ -9,6 +10,7 @@ const KakaoMap = () => {
 
         const onLoadKakaoAPI = () => {
             window.kakao.maps.load(() => {
+                console.log("kakao map api loaded")
                 const container = document.getElementById('map')
                 const options = {
                     center: new window.kakao.maps.LatLng(35.179587, 129.074857),
@@ -20,27 +22,38 @@ const KakaoMap = () => {
 
                 const geocoder = new window.kakao.maps.services.Geocoder();
 
-                geocoder.addressSearch('부산광역시 남구 대연동', function(result, status) {
-
-
+                if(selectedCity && selectedDistrict){
+                    const address = '${selectedCity} ${selectedDistrict}';
+                    console.log("address: ", address)
+                geocoder.addressSearch(address, function (result, status) {
                     if (status === window.kakao.maps.services.Status.OK) {
                         const coords = new window.kakao.maps.LatLng(result[0].y, result[0].x);
+                        console.log(" coords: ", coords);
                         const marker = new window.kakao.maps.Marker({
                             map: map,
                             position: coords,
                         });
                         const infowindow = new window.kakao.maps.InfoWindow({
-                            content: '<div style="width:150px;text-align:center;padding:6px 0;">여기</div>',
+                            content: '<div style="width:150px;text-align:center;padding:6px 0;"></div>',
                         });
-                        //infowindow.open(map, marker);
+                        infowindow.open(map, marker);
                         map.setCenter(coords);
+                    } else {
+                        console.error("Address search failed: ", status);
                     }
+
                 });
+            }
             });
+
         };
 
         kakaoMapScript.addEventListener('load', onLoadKakaoAPI);
-    }, []);
+        return() =>{
+            document.head.removeChild(kakaoMapScript);
+        };
+    }, [selectedCity,selectedDistrict]);
+
 
     return (
         <>
--- src/main/webapp/frontend/src/routes/Withme/Withme.jsx
+++ src/main/webapp/frontend/src/routes/Withme/Withme.jsx
@@ -3,13 +3,14 @@ import Modal from './Modal';
 import { Link } from 'react-router-dom';
 import WriteModal from './WriteModal'; // WriteModal 컴포넌트 import
 import '../../styles/Withme.css';
-import Map from './Map';
+import KakaoMap from './KakaoMap';
 
 
 function Withme() {
   const [selectedModal, setSelectedModal] = useState(null);
   const [isWriteModalOpen, setIsWriteModalOpen] = useState(false); // WriteModal 열림 상태를 관리하는 상태
   const [selectedCity, setSelectedCity] = useState(""); // 선택된 광역시 상태를 관리하는 상태
+  const [selectedDistrict, setSelectedDistrict] = useState(""); // 구 관리
 
   const dropdownOptions = {
     서울광역시: ['종로구','용산구', '성동구', '강북구', '서대문구', '마포구'],
@@ -28,8 +29,20 @@ function Withme() {
   };
 
   const handleCitySelect = (event) => {
-    setSelectedCity(event.target.value);
+
+    const selectedCity = event.target.value;
+    setSelectedCity(selectedCity);
+    setSelectedDistrict(""); //시변경될때 구 초기화
+    console.log("city: ", selectedCity);
+
   };
+
+  const handleDistrictSelect = (event) =>{
+    const selectedDistrict = event.target.value;
+    setSelectedDistrict(selectedDistrict);
+    console.log("gugu: ", selectedDistrict);
+
+  }
 
   // WriteModal을 닫는 함수
   const handleCloseWriteModal = () => {
@@ -48,8 +61,9 @@ function Withme() {
               ))}
             </select>
           </div>
+
           <div className={`dropdown-container ${selectedCity ? 'active' : ''}`}>
-            <select>
+            <select onChange={handleDistrictSelect} disabled={!selectedCity}>
               <option value="">구 선택</option>
               {selectedCity && dropdownOptions[selectedCity].map((option, index) => (
                 <option key={index} value={option}>{option}</option>
@@ -93,11 +107,11 @@ function Withme() {
       </section>
       <div className="divider"></div>
       <section className="right-section">
-        <div className="location-text">같이 갈 위치 : 부산 해운대
+        <div className="location-text">같이 갈 위치 : {selectedCity} {selectedDistrict}
           <button className="write-button" onClick={handleButtonClick}>글쓰기</button>
         </div>
         <div className="map-container">
-          <Map/>
+          <KakaoMap selectedCity={selectedCity} selectedDistrict={selectedDistrict} />
         </div>
       </section>
       {/* WriteModal이 열려있는 경우에만 렌더링 */}
